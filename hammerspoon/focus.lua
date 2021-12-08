f5f01e5f84d86fb5837bfe26955761eed11c6eea
Svapnil <svapnila@gmail.com>
2021-12-07 16:22:24 -0800

    prepared setup for MT tools

--- hammerspoon/focus.lua
+++ hammerspoon/focus.lua
@@ -12,15 +12,11 @@ hs.hotkey.bind(hyper, "C", function ()
 end)
 
 hs.hotkey.bind(hyper,  "Q", function()
-  hs.application.launchOrFocus('fb-vscode-insiders')
+  hs.application.launchOrFocus('Visual Studio Code')
 end)
 
 hs.hotkey.bind(hyper,  "E", function()
-  hs.application.launchOrFocus('Microsoft Outlook')
-end)
-
-hs.hotkey.bind(hyper,  "W", function()
-  hs.application.launchOrFocus('WhatsApp')
+  hs.application.launchOrFocus('Google Chrome')
 end)
 
 hs.hotkey.bind(hyper,  "M", function()
@@ -29,10 +25,6 @@ end)
 
 hs.hotkey.bind(hyper,  "O", function()
   hs.application.launchOrFocus('zoom.us')
-end)
-
-hs.hotkey.bind(hyper,  "X", function()
-  hs.application.launchOrFocus('Xcode')
 end)
 
 hs.hotkey.bind(hyper,  "R", function()
@@ -44,40 +36,7 @@ hs.hotkey.bind(hyper,  "space", function()
 end)
 
 hs.hotkey.bind(hyper, "I", function()
-  whatsapp  = hs.application.get("WhatsApp")
-  chat = hs.application.get("Workplace Chat")
-  local oldActive
-  local newActive
-  if hs.application.frontmostApplication() == whatsapp then
-    hs.application.launchOrFocus('Workplace Chat')
-    newActive = chat
-    oldActive = whatsapp
-  else
-    if hs.application.frontmostApplication() == chat then
-      hs.application.launchOrFocus('WhatsApp')
-      newActive = whatsapp
-      oldActive = chat
-    else
-      hs.application.launchOrFocus('Workplace Chat')
-    end
-  end
-  if oldActive and newActive then 
-    local win = oldActive:focusedWindow()
-    local id = win:id()
-    local screen = win:screen()
-    cell = hs.grid.get(win, screen)
-    local newWindow = newActive:focusedWindow()
-    hs.grid.set(newWindow, cell, screen)
-  end
-end)
-
-hs.hotkey.bind(hyper, "U", function()
-  messenger = hs.application.get("Messenger")
-  imessage = hs.application.get("Workplace Chat")
-  messenger:hide()
-  imessage:hide()
-  --- if Messenger is behind iMessage hide again
-  messenger:hide()
+  hs.application.launchOrFocus('Slack')
 end)
 
 hs.hotkey.bind(hyper, "Z", function()
@@ -89,5 +48,5 @@ hs.hotkey.bind(hyper, "1", function()
 end)
 
 hs.hotkey.bind(hyper, "N", function()
-  hs.application.launchOrFocus('Notes')
+  hs.application.launchOrFocus('Notion')
 end)
