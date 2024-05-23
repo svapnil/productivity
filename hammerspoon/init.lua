608a2576b1585986c925dd76e1d456f147255b6f
Svapnil <svapnila@gmail.com>
2024-05-23 16:34:38 -0400

    added paste

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,6 +10,7 @@ cwd = os.getenv("HOME") .. "/.hammerspoon/"
 -- Toggle modules here
 require('position')
 require('focus')
+require('paste')
 require('jump')
 require('monitor')
 require('airpods')
---
+++ hammerspoon/paste.lua
@@ -0,0 +1,4 @@
+
+hs.hotkey.bind(hyper,  "V", function()
+  hs.eventtap.keyStrokes("I'm working on a project called moonbot.gg that I'd love to show you. It's the first Solana trading app built inside Discord, and I'd love for you to try it out!")
+end)
